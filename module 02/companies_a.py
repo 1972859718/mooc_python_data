376f76ceb3fb0d337a1c31aa3fc022af719de62e
1972859718 <33173163+1972859718@users.noreply.github.com>
2018-04-15 14:28:37 +0800

    其他读写函数与例子

---
+++ module 02/companies_a.py
@@ -0,0 +1,4 @@
+#companies_a.py
+with open('companies.txt') as f:
+    cNames = f.readlines()
+print(cNames)
---
+++ module 02/recopy.py
@@ -0,0 +1,7 @@
+#recopy.py
+with open('companies.txt') as f1:
+    cNames = f1.readlines()
+for i in range(0, len(cNames)):
+    cNames[i] = str(i+1) + ' ' + cNames[i]
+    with open('scompanies.txt','w') as f2:
+        f2.writelines(cNames)
